d1005d0d93d8b731e4b667a277f5ae021ea429e9
Ryan Rojas <landoslair@gmail.com>
2023-03-13 17:58:33 -0400

    updated text

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript.";
+h2.textContent = "This content is in development.";
 document.querySelector("body").appendChild(h2);
 
 const h3 = document.createElement("h3");
-h3.textContent = "Why, you ask? Not sure! Stay tuned to find out :)";
+h3.textContent = "Check back frequently to see what has changed :)";
 document.querySelector("body").appendChild(h3);
